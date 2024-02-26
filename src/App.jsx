dd04b9a533426c56885d29228bc6a59f86203dd0
Sameer <ccontact.sameer04@gmail.com>
2024-02-26 19:26:43 +0530

    add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StudentList from './components/Students'; // Import StudentList component
 import StudentDetails from './components/StudentDetails'; // Import StudentDetails component
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<StudentList />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/studentList" element={<StudentList />} />
         <Route path="/students/:id" element={<StudentDetails />} />
         {/* Add other routes if needed */}
       </Routes>
